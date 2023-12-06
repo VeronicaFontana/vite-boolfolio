77a906ba338d3084af90fee74b9f9036d6e061b6
VeronicaFontana <veronyc98@hotmail.it>
2023-12-06 15:39:34 +0100

    [ADD] getApi method

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script>
 
+import axios from "axios";
+import {store} from "./data/store";
+
 export default{
   name: "App",
   data(){
     return{
     }
+  },
+  methods:{
+    getApi(){
+      axios.get(store.apiUrl + "projects")
+        .then(results =>{
+          console.log(results.data);
+        })
+    }
+  },
+  mounted(){
+    this.getApi();
   }
 }
 
